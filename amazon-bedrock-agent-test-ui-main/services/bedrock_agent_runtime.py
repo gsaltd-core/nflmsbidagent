9238976ca825a68720d9dee4a7090793745aa6d6
gsaltd-core <55751664+gsaltd-core@users.noreply.github.com>
2025-05-19 12:13:15 +0100

    Update bedrock_agent_runtime.py

--- amazon-bedrock-agent-test-ui-main/services/bedrock_agent_runtime.py
+++ amazon-bedrock-agent-test-ui-main/services/bedrock_agent_runtime.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 def invoke_agent(agent_id, agent_alias_id, session_id, prompt):
     try:
-        client = boto3.session.Session().client(service_name="bedrock-agent-runtime", region_name='us-east-1')               #1 Add Region Name
+        client = boto3.session.Session().client(service_name="bedrock-agent-runtime", region_name='eu-west-2')               #1 Add Region Name
         # See https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/invoke_agent.html
         response = client.invoke_agent(
             agentId='IFF1J6DPCM',                     #2 Add Agent ID
